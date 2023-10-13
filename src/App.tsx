fcee8d9336a043dd15d166c06cbd4cbdf9b6bf46
shahriar-programmer <shovonshahriar01@gmail.com>
2023-10-13 16:03:48 +0600

    Added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Quiz from "./views/Quiz/Quiz"
 import Result from "./views/Result/Result"
 import { Container } from "@mantine/core";
 import Splash from "./views/Splash/Splash";
+import { Analytics } from '@vercel/analytics/react';
 
 export type ScreenViewType = "splash" | "quiz" | "result"
 
@@ -12,20 +13,12 @@ function App() {
 
   return (
     <>
-      <Container h={"100vh"} w="100%" maw={"450px"} p={0} style={{overflow: "hidden"}} pos="relative">
+      <Container h={"100vh"} w="100%" maw={"450px"} p={0} style={{ overflow: "hidden" }} pos="relative">
         <Splash screenView={screenView} setScreenView={setScreenView} />
         <Quiz screenView={screenView} setScore={setScore} setScreenView={setScreenView} />
         <Result screenView={screenView} setScreenView={setScreenView} score={score} />
-        {/* {
-          screenView === "quiz" ?
-            <Quiz setScore={setScore} setScreenView={setScreenView} />
-            :
-            screenView == "result" ?
-              <Result setScreenView={setScreenView} score={score} />
-              :
-              <Splash setScreenView={setScreenView} />
-        } */}
       </Container>
+      <Analytics />
     </>
   )
 }
